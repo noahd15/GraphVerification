beec52dbf088a1e1aebc0e1c0817b8cd6b8cb5b3
Noah Dahle <noah.dahle@vanderbilt.edu>
2025-04-20 11:56:30 -0400

    Dynamically adding epsilons

--- node_verification/CORA_Original/process_results.m
+++ node_verification/CORA_Original/process_results.m
@@ -7,8 +7,8 @@
 %% Process results for each model independently
 
 % seeds = [0,1,2,3,4]; % models
-seeds = [1] %[5,6,7,8,9]; % models
-epsilon = [0.005] %[0.005; 0.01; 0.02; 0.05];
+seeds = [1]; %[5,6,7,8,9]; % models
+epsilon = [0.005, .01]; %[0.005; 0.01; 0.02; 0.05];
 eN = length(epsilon);
 
 % Verify one model at a time
@@ -18,7 +18,6 @@ for m=1:length(seeds)
     modelPath = "cora_node_gcn_"+string(seeds(m));
     
     % initialize vars
-    molecules = zeros(eN,4); % # robust, #unknown, # not robust/misclassified, # molecules
     atoms = zeros(eN,4);     % # robust, #unknown, # not robust/misclassified, # atoms
     
     for k = 1:eN
@@ -36,16 +35,7 @@ for m=1:length(seeds)
             unk = sum(res==2); % unknown
             nrb = sum(res==0); % not robust
             
-            % molecules
-            if rb == n
-                molecules(k,1) = molecules(k,1) + 1;
-            elseif unk == n
-                molecules(k,2) = molecules(k,2) + 1;
-            elseif nrb == n
-                molecules(k,3) = molecules(k,3) + 1;
-            end
-            molecules(k,4) = molecules(k,4) + 1;
-            
+
             % atoms
             atoms(k,1) = atoms(k,1) + rb;
             atoms(k,2) = atoms(k,2) + unk;
@@ -62,22 +52,17 @@ for m=1:length(seeds)
     model = load("models/"+modelPath+".mat");
     
     % Create table with these values
-    % no need for the molecules, as we could not fully verify any of them
     fileID = fopen("verification_results/summay_results_Linf_"+modelPath+".txt",'w');
     fprintf(fileID, 'Summary of robustness results of gnn model with accuracy = %.4f \n\n', model.testAcc);
-    fprintf(fileID, '               MOLECULES \n');
-    fprintf(fileID, 'Epsilon | Robust  Unknown  Not Rob.  N \n');
-    fprintf(fileID, '  0.1 | %.3f    %.3f   %.3f   %d \n', molecules(1,1)/molecules(1,4), molecules(1,2)/molecules(1,4), molecules(1,3)/molecules(1,4), molecules(1,4));
-    % fprintf(fileID, '   0.01 | %.3f    %.3f   %.3f   %d \n', molecules(2,1)/molecules(2,4), molecules(2,2)/molecules(2,4), molecules(2,3)/molecules(2,4), molecules(2,4));
-    % fprintf(fileID, '   0.02 | %.3f    %.3f   %.3f   %d \n', molecules(3,1)/molecules(3,4), molecules(3,2)/molecules(3,4), molecules(3,3)/molecules(3,4), molecules(3,4));
-    % fprintf(fileID, '   0.05 | %.3f    %.3f   %.3f   %d \n', molecules(4,1)/molecules(4,4), molecules(4,2)/molecules(4,4), molecules(4,3)/molecules(4,4), molecules(4,4));
-    % fprintf(fileID, ' \n\n');
-    % fprintf(fileID,'                 ATOMS \n');
-    % fprintf(fileID, 'Epsilon | Robust  Unknown  Not Rob.  N \n');
-    % fprintf(fileID, '  0.005 | %.3f    %.3f   %.3f   %d \n', atoms(1,1)/atoms(1,4), atoms(1,2)/atoms(1,4), atoms(1,3)/atoms(1,4), atoms(1,4));
-    % fprintf(fileID, '   0.01 | %.3f    %.3f   %.3f   %d \n', atoms(2,1)/atoms(2,4), atoms(2,2)/atoms(2,4), atoms(2,3)/atoms(2,4), atoms(2,4));
-    % fprintf(fileID, '   0.02 | %.3f    %.3f   %.3f   %d \n', atoms(3,1)/atoms(3,4), atoms(3,2)/atoms(3,4), atoms(3,3)/atoms(3,4), atoms(3,4));
-    % fprintf(fileID, '   0.05 | %.3f    %.3f   %.3f   %d \n', atoms(4,1)/atoms(4,4), atoms(4,2)/atoms(4,4), atoms(4,3)/atoms(4,4), atoms(4,4));
+
+
+
+    % Print ATOMS table dynamically
+    fprintf(fileID, '               CORA\n');
+    fprintf(fileID, 'Epsilon | Robust    Unknown   Not Rob.   Count \n');
+    for k = 1:eN
+        fprintf(fileID, ' %8.4f | %.3f    %.3f    %.3f    %d \n', epsilon(k), atoms(k,1)/atoms(k,4), atoms(k,2)/atoms(k,4), atoms(k,3)/atoms(k,4), atoms(k,4));
+    end
     fclose(fileID);
 
 end
